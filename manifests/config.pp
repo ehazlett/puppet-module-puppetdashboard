1a1a82c90618ac138d027e9e2fe904aeaf6f20b0
Evan Hazlett <ejhazlett@gmail.com>
2012-06-28 09:12:50 -0400

    updated to use puppetlabs deb for apt repo

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,28 +4,10 @@ class puppetdashboard::config inherits puppetdashboard::params {
     logoutput => on_failure,
   }
 
-  file { "puppetdashboard::config::puppetlabs_apt_repo_config":
-    path    => "/etc/apt/sources.list.d/puppetlabs.list",
-    content => template("puppetdashboard/apt_puppetlabs.list.erb"),
-    owner   => "root",
-    group   => "root",
-    mode    => 0644,
-    notify  => Exec["puppetdashboard::config::get_puppetlabs_apt_key"],
-  }
-  exec { "puppetdashboard::config::get_puppetlabs_apt_key":
-    command     => "gpg --keyserver keys.gnupg.net --recv-key 4BD6EC30",
-    user        => root,
-    timeout     => 600,
-    refreshonly => true,
-    notify      => Exec["puppetdashboard::config::import_puppetlabs_apt_key"],
-  }
-  exec { "puppetdashboard::config::import_puppetlabs_apt_key":
-    command     => "gpg -a --export 4BD6EC30 | apt-key add -",
-    user        => root,
-    timeout     => 600,
-    refreshonly => true,
-    require     => Exec["puppetdashboard::config::get_puppetlabs_apt_key"],
-    notify      => Exec["puppetdashboard::config::update_apt"],
+  exec { "puppetdashboard::config::puppetlabs_apt_repo_config":
+    cwd     => "/tmp"
+    command => "wget wget http://apt.puppetlabs.com/puppetlabs-release_1.0-3_all.deb -O puppetlabs.deb; dpkg -i puppetlabs.deb"
+    notify  => Exec["puppetdashboard::config::update_apt"],
   }
   exec { "puppetdashboard::config::update_apt":
     command     => "apt-get -y update",

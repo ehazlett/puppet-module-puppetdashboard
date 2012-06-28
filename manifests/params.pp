c7fe1e9eaf36d06f84129b5068e60394f8c018da
Evan Hazlett <ejhazlett@gmail.com>
2012-06-28 09:42:53 -0400

    added param

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,6 @@
 class puppetdashboard::params {
   $rubygems_url = "http://production.cf.rubygems.org/rubygems/rubygems-1.3.7.tgz"
+  $config_mysql = true
   $dashboard_db_name = "dashboard"
   $dashboard_db_username = "dashboard"
   $dashboard_db_password = "d@5hB0ard"
